4702d775890881d8a2276176f886c7ad9ab6afa0
ratabb <ratabb@users.noreply.github.com>
2023-08-05 12:43:13 +0700

    1st-commit

---
+++ sing-box/main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/sagernet/sing-box/common/geosite"
+	"github.com/sagernet/sing/common"
+	"github.com/sirupsen/logrus"
+	"github.com/v2fly/v2ray-core/v5/app/router/routercommon"
+	"google.golang.org/protobuf/proto"
+)
+
+func parse(vGeositeData []byte) (map[string][]geosite.Item, error) {
+	vGeositeList := routercommon.GeoSiteList{}
+	err := proto.Unmarshal(vGeositeData, &vGeositeList)
+	if err != nil {
+		return nil, err
+	}
+	domainMap := make(map[string][]geosite.Item)
+	for _, vGeositeEntry := range vGeositeList.Entry {
+		code := strings.ToLower(vGeositeEntry.CountryCode)
+		domains := make([]geosite.Item, 0, len(vGeositeEntry.Domain)*2)
+		attributes := make(map[string][]*routercommon.Domain)
+		for _, domain := range vGeositeEntry.Domain {
+			if len(domain.Attribute) > 0 {
+				for _, attribute := range domain.Attribute {
+					attributes[attribute.Key] = append(attributes[attribute.Key], domain)
+				}
+			}
+			switch domain.Type {
+			case routercommon.Domain_Plain:
+				domains = append(domains, geosite.Item{
+					Type:  geosite.RuleTypeDomainKeyword,
+					Value: domain.Value,
+				})
+			case routercommon.Domain_Regex:
+				domains = append(domains, geosite.Item{
+					Type:  geosite.RuleTypeDomainRegex,
+					Value: domain.Value,
+				})
+			case routercommon.Domain_RootDomain:
+				if strings.Contains(domain.Value, ".") {
+					domains = append(domains, geosite.Item{
+						Type:  geosite.RuleTypeDomain,
+						Value: domain.Value,
+					})
+				}
+				domains = append(domains, geosite.Item{
+					Type:  geosite.RuleTypeDomainSuffix,
+					Value: "." + domain.Value,
+				})
+			case routercommon.Domain_Full:
+				domains = append(domains, geosite.Item{
+					Type:  geosite.RuleTypeDomain,
+					Value: domain.Value,
+				})
+			}
+		}
+		domainMap[code] = common.Uniq(domains)
+		for attribute, attributeEntries := range attributes {
+			attributeDomains := make([]geosite.Item, 0, len(attributeEntries)*2)
+			for _, domain := range attributeEntries {
+				switch domain.Type {
+				case routercommon.Domain_Plain:
+					attributeDomains = append(attributeDomains, geosite.Item{
+						Type:  geosite.RuleTypeDomainKeyword,
+						Value: domain.Value,
+					})
+				case routercommon.Domain_Regex:
+					attributeDomains = append(attributeDomains, geosite.Item{
+						Type:  geosite.RuleTypeDomainRegex,
+						Value: domain.Value,
+					})
+				case routercommon.Domain_RootDomain:
+					if strings.Contains(domain.Value, ".") {
+						attributeDomains = append(attributeDomains, geosite.Item{
+							Type:  geosite.RuleTypeDomain,
+							Value: domain.Value,
+						})
+					}
+					attributeDomains = append(attributeDomains, geosite.Item{
+						Type:  geosite.RuleTypeDomainSuffix,
+						Value: "." + domain.Value,
+					})
+				case routercommon.Domain_Full:
+					attributeDomains = append(attributeDomains, geosite.Item{
+						Type:  geosite.RuleTypeDomain,
+						Value: domain.Value,
+					})
+				}
+			}
+			domainMap[code+"@"+attribute] = common.Uniq(attributeDomains)
+		}
+	}
+	return domainMap, nil
+}
+
+func local(input string, output string) error {
+	outputFile, err := os.Create(output)
+	if err != nil {
+		return err
+	}
+	defer outputFile.Close()
+	binary, err := os.ReadFile(input)
+	if err != nil {
+		return err
+	}
+	domainMap, err := parse(binary)
+	if err != nil {
+		return err
+	}
+	outputPath, _ := filepath.Abs(output)
+	os.Stderr.WriteString("write " + outputPath + "\n")
+	return geosite.Write(outputFile, domainMap)
+}
+
+func main() {
+	err := local(os.Args[1], os.Args[2])
+	if err != nil {
+		logrus.Fatal(err)
+	}
+}
